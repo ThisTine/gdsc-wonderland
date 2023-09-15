aefcad21e655fd272bce7207df4e0a09c3a2a63a
sittichok Ouamsiri <sutichok46@gmail.com>
2023-09-16 01:02:24 +0700

    add bg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import {
     Box,
     Button,
     Center,
-    Container,
+    Container, Heading, HStack, Link,
     Modal, ModalBody, ModalCloseButton,
     ModalContent, ModalFooter,
     ModalHeader,
@@ -10,9 +10,32 @@ import {
 } from "@chakra-ui/react";
 import {Game} from "./Game.tsx";
 import {NavBar} from "./Component/NavBar.tsx";
+import {useEffect, useState} from "react";
+import bg from './assets/bg.jpg'
+
+function hash(string:string) {
+    const utf8 = new TextEncoder().encode(string);
+    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray
+            .map((bytes) => bytes.toString(16).padStart(2, '0'))
+            .join('');
+        return hashHex;
+    });
+}
 
 function App() {
     const {isOpen,onClose, onOpen} = useDisclosure()
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    const [hashedToken,setHashedToken] = useState("")
+    useEffect(() => {
+        if(token){
+            hash(token).then((hash) => {
+                setHashedToken(hash)
+            })
+        }
+    },[token])
     return (
         <>
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -27,15 +50,20 @@ function App() {
                     <Text mt={5}>
                         โดยเกมนี้คาดหวังให้เราได้คุยกับเพื่อน ๆ ใหม่ ๆ และได้ลองทำกิจกรรมร่วมกัน !
                     </Text>
+                    <HStack>
+                        <Button colorScheme="facebook" as={"a"} ><Link href={"https://www.facebook.com/gdsc.kmutt"} target={"_blank"} >Facebook</Link></Button>
+                        <Button colorScheme="pink" as={"a"} ><Link href={"https://www.instagram.com/gdsc.kmutt"} target={"_blank"} >Instagram</Link></Button>
+
+                    </HStack>
                 </ModalBody>
                 <ModalFooter/>
             </ModalContent>
         </Modal>
-        <Box minW={"100vw"} minH={"100vh"}>
+        <Box color={"white"} minW={"100vw"} backdropFilter={"blur(10px)"} bgImage={bg} backgroundSize={"cover"} minH={"100vh"}>
             <NavBar/>
             <Center>
                 <Container maxW={"container.xl"}>
-                    <Game/>
+                    {hashedToken ? <Game token={hashedToken}/> : token?<Heading textAlign={"center"}>Loading</Heading> : <Heading textAlign={"center"}>Token not found, please check the link from google form</Heading>}
                 </Container>
             </Center>
 
--- frontend/src/Component/NavBar.tsx
+++ frontend/src/Component/NavBar.tsx
@@ -2,13 +2,13 @@ import {Box, Center, Container, Heading, HStack, Image} from "@chakra-ui/react";
 
 export const NavBar = () => {
 	return (
-		<Box w={"100%"} shadow={"md"} py={3}>
+		<Box w={"100%"} background={"white"} shadow={"md"} py={3}>
 			<Center>
 				<Container maxW={"container.xl"}>
 					<HStack>
 						<Image w={"80px"}
 							   src={"https://raw.githubusercontent.com/nimishbongale/DSC-RIT_Recruitment_Test/master/static/dscnew.png?token=AKLHGWOKT4B6YUSLBUNLU526NEMIS"}/>
-						<Heading size={"md"}>GDSC  KMUTT</Heading>
+						<Heading color={"black"} size={"md"}>GDSC  KMUTT</Heading>
 					</HStack>
 				</Container>
 			</Center>
